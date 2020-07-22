a08f239ddf046b81177bccfe0c330e2936e6d705
justineschott <justine@educationsuperhighway.org>
2020-07-22 12:26:38 -0700

    updating faq class names to be specific to faq

--- components/Faq.vue
+++ components/Faq.vue
@@ -2,19 +2,19 @@
   <div class="Faq">
     <div class="Faq-Header">Frequently Asked Questions</div>
     <div class="Faq-Question1">
-      <div class="Question">Where does all this data come from?</div>
-      <div class="Expand">+</div>
+      <div class="Faq-Question">Where does all this data come from?</div>
+      <div class="Faq-Expand">+</div>
     </div>
     <div class="Faq-Question2">
-      <div class="Question-Multi">
+      <div class="Faq-Question-Multi">
         When are upcoming elections?
         Which races have an impact on police spending, or policing in general?
       </div>
-      <div class="Expand">+</div>
+      <div class="Faq-Expand">+</div>
     </div>
     <div class="Faq-Question3">
-      <div class="Question">Who can I call or write to voice my opinion?</div>
-      <div class="Expand">+</div>
+      <div class="Faq-Question">Who can I call or write to voice my opinion?</div>
+      <div class="Faq-Expand">+</div>
     </div>
   </div>
 </template>
@@ -75,45 +75,45 @@
     border: 1px solid #bdbdbd;
     box-sizing: border-box;
   }
-}
-.Question {
-  position: absolute;
-  width: 720px;
-  height: 25px;
-  left: 26px;
-  top: 30px;
-  font-family: Avenir;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 25px;
-  color: #000000;
-}
-.Question-Multi {
-  position: absolute;
-  width: 720px;
-  height: 25px;
-  left: 26px;
-  top: 16px;
-  font-family: Avenir;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 25px;
-  color: #000000;
-}
-.Expand {
-  position: absolute;
-  width: 24px;
-  height: 49px;
-  left: 768px;
-  top: 18px;
-  font-family: Avenir;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 36px;
-  line-height: 49px;
-  color: #000000;
+  &-Question {
+    position: absolute;
+    width: 720px;
+    height: 25px;
+    left: 26px;
+    top: 30px;
+    font-family: Avenir;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 25px;
+    color: #000000;
+  }
+  &-Question-Multi {
+    position: absolute;
+    width: 720px;
+    height: 25px;
+    left: 26px;
+    top: 16px;
+    font-family: Avenir;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 25px;
+    color: #000000;
+  }
+  &-Expand {
+    position: absolute;
+    width: 24px;
+    height: 49px;
+    left: 768px;
+    top: 18px;
+    font-family: Avenir;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 36px;
+    line-height: 49px;
+    color: #000000;
+  }
 }
 
 @include lessThan($small) {
