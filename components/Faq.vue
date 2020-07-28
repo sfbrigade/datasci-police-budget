3d5c69a0615bc1d11a87ee46db3adf883677ea77
justineschott <justine@educationsuperhighway.org>
2020-07-27 17:57:07 -0700

    adding colors to variables file and reducing redundant css coding

--- components/Faq.vue
+++ components/Faq.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="Faq">
     <div class="Faq-Header">Frequently Asked Questions</div>
-    <div class="Faq-Question1">
-      <div class="Faq-Question">Where does all this data come from?</div>
-      <div class="Faq-Expand">+</div>
+    <div class="Faq-Question-Q1">
+      <div class="Faq-Question-Text">Where does all this data come from?</div>
+      <div class="Faq-Question-Expand">+</div>
     </div>
-    <div class="Faq-Question2">
-      <div class="Faq-Question-Multi">
+    <div class="Faq-Question-Q2">
+      <div class="Faq-Question-Text-Multi">
         When are upcoming elections?
         Which races have an impact on police spending, or policing in general?
       </div>
-      <div class="Faq-Expand">+</div>
+      <div class="Faq-Question-Expand">+</div>
     </div>
-    <div class="Faq-Question3">
-      <div class="Faq-Question">Who can I call or write to voice my opinion?</div>
-      <div class="Faq-Expand">+</div>
+    <div class="Faq-Question-Q3">
+      <div class="Faq-Question-Text">Who can I call or write to voice my opinion?</div>
+      <div class="Faq-Question-Expand">+</div>
     </div>
   </div>
 </template>
@@ -30,89 +30,72 @@
   height: 497px;
   left: 0px;
   top: 936px;
-  background: #f1f8f8;
+  background: $light-turquoise;
+  color: $black-1;
+  font-family: "Avenir";
+  font-style: normal;
+  font-weight: normal;
   &-Header {
     position: absolute;
     width: 553px;
     height: 33px;
     left: 443px;
     top: 64px;
-    font-family: Avenir;
-    font-style: normal;
-    font-weight: 800;
     font-size: 24px;
     line-height: 33px;
     text-align: center;
-    color: #000000;
-  }
-  &-Question1 {
-    position: absolute;
-    width: 819px;
-    height: 84px;
-    left: 311px;
-    top: 137px;
-    background: #ffffff;
-    border: 1px solid #bdbdbd;
-    box-sizing: border-box;
-  }
-  &-Question2 {
-    position: absolute;
-    width: 819px;
-    height: 84px;
-    left: 311px;
-    top: 220px;
-    background: #ffffff;
-    border: 1px solid #bdbdbd;
-    box-sizing: border-box;
-  }
-  &-Question3 {
-    position: absolute;
-    width: 819px;
-    height: 84px;
-    left: 311px;
-    top: 303px;
-    background: #ffffff;
-    border: 1px solid #bdbdbd;
-    box-sizing: border-box;
   }
   &-Question {
     position: absolute;
-    width: 720px;
-    height: 25px;
-    left: 26px;
-    top: 30px;
-    font-family: Avenir;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 18px;
-    line-height: 25px;
-    color: #000000;
-  }
-  &-Question-Multi {
-    position: absolute;
-    width: 720px;
-    height: 25px;
-    left: 26px;
-    top: 16px;
-    font-family: Avenir;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 18px;
-    line-height: 25px;
-    color: #000000;
-  }
-  &-Expand {
-    position: absolute;
-    width: 24px;
-    height: 49px;
-    left: 768px;
-    top: 18px;
-    font-family: Avenir;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 36px;
-    line-height: 49px;
-    color: #000000;
+    background: $white-1;
+    border: 1px solid $gray-5;
+    box-sizing: border-box;
+    &-Q1 {
+      position: absolute;
+      width: 819px;
+      height: 84px;
+      left: 311px;
+      top: 137px;
+    }
+    &-Q2 {
+      position: absolute;
+      width: 819px;
+      height: 84px;
+      left: 311px;
+      top: 220px;
+    }
+    &-Q3 {
+      position: absolute;
+      width: 819px;
+      height: 84px;
+      left: 311px;
+      top: 303px;
+    }
+    &-Text {
+      position: absolute;
+      width: 720px;
+      height: 25px;
+      left: 26px;
+      top: 30px;
+      font-size: 18px;
+      line-height: 25px;
+      &-Multi {
+        position: absolute;
+        width: 720px;
+        left: 26px;
+        top: 16px;
+        font-size: 18px;
+      }
+    }
+    &-Expand {
+      position: absolute;
+      width: 24px;
+      height: 49px;
+      left: 768px;
+      top: 18px;
+      font-size: 36px;
+      line-height: 49px;
+    }
   }
 }
 
