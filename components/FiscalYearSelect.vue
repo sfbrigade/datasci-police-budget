8bc1511bcd77fe86f4909a91c48cc3f804d69386
James Baskerville <jameswbaskerville@gmail.com>
2020-09-16 20:21:26 -0700

    feat: Add app-wide state for city and year choice
    Using Vuex (already included in Nuxt) will allow us to track app-wide state
    for city and fiscal year choice and prepopulate across selectors on different
    pages, while still allowing users to change them via any of those selectors.
    
    Also refactors selectors into shared components.
    
    Issue #30.

---
+++ components/FiscalYearSelect.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <label for="fiscal-year-select">Fiscal Year</label>
+    <v-select id="fiscal-year-select"
+      background-color="white"
+      color="black"
+      v-model="selectedFiscalYear"
+      :items="years"
+      light
+      outlined
+      dense>
+    </v-select>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  computed: {
+    selectedFiscalYear: {
+      get() { return this.$store.state.year; },
+      set(value) { this.$store.commit('updateFiscalYear', value); },
+    }    
+  },
+  data() {
+    return {
+      years: [
+        {
+          text: '2020-2021',
+          value: '2020',
+          disabled: false,
+        },
+        {
+          text: '2019-2020',
+          value: '2019',
+          disabled: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped lang="scss">
+  label {
+    @include font-size(12);
+  }
+</style>  
